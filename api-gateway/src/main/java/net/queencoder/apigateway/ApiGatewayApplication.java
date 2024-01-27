219a60fbe45a51d59ba50cb6cac17a035ea562fe
juliettegodyere <juliettejuli@yahoo.com>
2024-01-27 21:58:39 +0000

    Register API Gateway as Eureka client to eureka server

--- api-gateway/src/main/java/net/queencoder/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/net/queencoder/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,16 @@ package net.queencoder.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication application = new SpringApplication(ApiGatewayApplication.class);
+		application.setAdditionalProfiles("local");
+		application.run(args);
 	}
 
 }
