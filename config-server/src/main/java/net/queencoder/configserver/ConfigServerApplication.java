bf3b3175cc61bef975241ba3bac16787a59417d9
juliettegodyere <juliettejuli@yahoo.com>
2024-01-28 21:15:43 +0000

    Register Config-Server as Eureka Client

--- config-server/src/main/java/net/queencoder/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/net/queencoder/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package net.queencoder.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
