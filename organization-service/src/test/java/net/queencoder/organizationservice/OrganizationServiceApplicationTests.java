9c1acad2dd61059cf1cba5ad048539c2eb9bddf9
juliettegodyere <juliettejuli@yahoo.com>
2024-01-29 23:14:53 +0000

    Add the organization service

---
+++ organization-service/src/test/java/net/queencoder/organizationservice/OrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.queencoder.organizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
