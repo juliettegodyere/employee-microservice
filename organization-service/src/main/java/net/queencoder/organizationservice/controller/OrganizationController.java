f7ced511cbf40ef3b52798859cd526ed69a090f3
juliettegodyere <juliettejuli@yahoo.com>
2024-02-02 15:44:33 +0000

    Creat API documentation for Organisation service

--- organization-service/src/main/java/net/queencoder/organizationservice/controller/OrganizationController.java
+++ organization-service/src/main/java/net/queencoder/organizationservice/controller/OrganizationController.java
@@ -9,10 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import net.queencoder.organizationservice.dto.OrganizationDto;
 import net.queencoder.organizationservice.service.OrganizationService;
 
+@Tag(
+    name = "Organisation Service - OrganisationController",
+    description = "Organisation Controller Exposes REST APIs for Organisation-Service"
+)
 @RestController
 @RequestMapping("api/organizations")
 @AllArgsConstructor
@@ -20,6 +27,14 @@ public class OrganizationController {
 
     private final OrganizationService organizationService;
 
+    @Operation(
+        summary = "Save Organisation REST API",
+        description = " Save Organisation REST API is used to save organisation object in a database"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto){
         OrganizationDto saveOrganizationDto = organizationService.saveOrganization(organizationDto);
@@ -27,6 +42,14 @@ public class OrganizationController {
         return new ResponseEntity<>(saveOrganizationDto, HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "Get Organisation REST API",
+        description = " Get Organisation REST API is used to get organisation object from th database"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "HTTP Status 201 CREATED"
+    )
     @GetMapping("{code}")
     public ResponseEntity<OrganizationDto> getOrganizationByCode(@PathVariable("code") String code){
         OrganizationDto saveOrganizationDto = organizationService.getOrganizationByCode(code);
