3e7286559c235ce41162e8f4e03816a730f1aa80
juliettegodyere <juliettejuli@yahoo.com>
2024-02-02 15:24:10 +0000

    Creat API documentation for Department service

--- department-service/src/main/java/net/queencoder/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/net/queencoder/departmentservice/controller/DepartmentController.java
@@ -9,10 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import net.queencoder.departmentservice.dto.DepartmentDto;
 import net.queencoder.departmentservice.service.DepartmentService;
 
+@Tag(
+    name = "Department Service - DepartmentController",
+    description = "Department Controller Exposes REST APIs for Department-Service"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/departments")
@@ -20,11 +27,27 @@ public class DepartmentController {
     
     private DepartmentService departmentService;
 
+    @Operation(
+        summary = "Save Department REST API",
+        description = " Save Department REST API is used to save department object in a database"
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
         return new ResponseEntity<>(departmentService.saveDepartment(departmentDto), HttpStatus.CREATED);
     }
-    
+
+     @Operation(
+        summary = "Get Department REST API",
+        description = " Get Department REST API is used to get department object from the database"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("/{department-code}")
     public ResponseEntity<DepartmentDto> getDepartmentByCode(@PathVariable("department-code") String departmentCode) {
         DepartmentDto savedDepartment = departmentService.getDepartmentByCode(departmentCode);
--- department-service/src/main/java/net/queencoder/departmentservice/dto/DepartmentDto.java
+++ department-service/src/main/java/net/queencoder/departmentservice/dto/DepartmentDto.java
@@ -1,10 +1,14 @@
 package net.queencoder.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+    description = "Department Model Information"
+)
 @Setter
 @Getter
 @AllArgsConstructor
@@ -13,9 +17,18 @@ public class DepartmentDto {
     
     private Long id;
 
+    @Schema(
+        description = "Department Name"
+    )
     private String departmentName;
 
+    @Schema(
+        description = "Department Description"
+    )
     private String departmentDescription;
-    
+
+    @Schema(
+        description = "Department Code"
+    )
     private String departmentCode;
 }
